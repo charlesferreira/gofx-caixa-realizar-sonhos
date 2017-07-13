711f51f4fd59aaf286d134804f9a4cce049dbddf
Charles Ferreira <charles.dev@live.com>
2017-07-13 15:13:09 -0300

    Peça finalizada v1.0

--- Common/Ad.js
+++ Common/Ad.js
@@ -1,76 +1,192 @@
 var Ad = function(options) {
-    this.setInlineStyle = gwd.actions.events.setInlineStyle;
-
     this.setup(options, {
         width: 300,
         height: 250,
         numPictures: 4,
+        frameDuration: 1100,
+        arrowOpacityOn: 0.825,
+        arrowOpacityOff: 0.53,
+        autoPlayTimeOut: 5000,
+        autoPlayInterval: 2500,
+        autoPlayMaxTimeOut: 20000,
         effects: {
-            overlayFadeOut: {delay: 2000, transition: '0.5s ease-in'},
-            arrowsSlideIn: {delay: 1500, transition: '1.0s ease-out'}
+            overlayFadeOut: { transition: '0.5s ease-in', delay: 2000 },
+            arrowsSlideIn: { transition: '1.0s ease-out', delay: 1500 },
+            arrowsSlideOut: { transition: '1.0s ease-in' },
+            arrowLeftFade: { transition: '0.5s ease-out' },
+            framePhotoSlideIn: { transition: '0.8s ease-in-out' },
+            framePhotoSlideOut: { transition: '0.8s ease-in-out' },
+            frameTextSlideIn: { transition: '0.4s ease-out', delay: 600 },
+            frameTextSlideOut: { transition: '0.35s ease-in' },
+            underlayCrossFade: { transition: '0.5s ease-in-out', delay: 2500 }
         },
-    });
-
-    this.start();
+    }).start();
 }
 
 Ad.prototype.setup = function(options, defaultOptions) {
     for (var i in defaultOptions)
         this[i] = options[i] || defaultOptions[i];
-    this.setupTexts();
-    this.setupPhotos();
-    this.setupArrows();
+    this.currentFrame = 0;
+    return this
+        .setupTexts()
+        .setupPhotos()
+        .setupArrows()
+        .setupAutoPlay();
+}
+
+Ad.prototype.setupAutoPlay = function() {
+    setTimeout(function() {
+        if (!this.isAutoPlaying)
+            this.autoPlay();
+        this.slideOutArrows();
+    }.bind(this), this.autoPlayMaxTimeOut);
+    return this.resetAutoPlay();
+}
+
+Ad.prototype.resetAutoPlay = function() {
+    if (this.autoPlayTimeOutId) clearTimeout(this.autoPlayTimeOutId);
+    if (this.autoPlayIntervalId) clearInterval(this.autoPlayIntervalId);
+    this.autoPlayTimeOutId = setTimeout(this.autoPlay.bind(this), this.autoPlayTimeOut);
+    this.isAutoPlaying = false;
+    return this;
+}
+
+Ad.prototype.autoPlay = function() {
+    this.autoPlayIntervalId = setInterval(this.navigate.bind(this, +1), this.autoPlayInterval);
+    this.isAutoPlaying = true;
+    return this.navigate(+1);
 }
 
 Ad.prototype.setupPhotos = function() {
     for (var i = 1; i < this.numPictures; i++)
-        this.setInlineStyle('pic-' + i, 'transform: translateX(' + this.width + 'px)');
+        this.slideOutRight('pic-' + i);
+    return this
+        .fadeOut('underlay-logo');
 }
 
 Ad.prototype.setupTexts = function() {
-    for (var i = 0; i < this.numPictures; i++)
-        this.setInlineStyle('txt-' + i, 'opacity: 0');
+    for (var i = 1; i < this.numPictures; i++)
+        this.slideOutRight('txt-' + i);
+    return this
+        .fadeOut('txt-0')
+        .fadeIn('txt-0', this.effects.overlayFadeOut);
 }
 
 Ad.prototype.setupArrows = function() {
-    this.setInlineStyle('arr-left', 'transform: translateX(-' + this.width + 'px)');
-    this.setInlineStyle('arr-right', 'transform: translateX(' + this.width + 'px)');
+    return this
+        .slideOutLeft('arr-left')
+        .slideOutRight('arr-right')
+        .slideIn('arr-right', this.effects.arrowsSlideIn)
+        .slideIn('arr-left', this.effects.arrowsSlideIn)
+        .fadeTo(this.arrowOpacityOff, 'arr-left')
+        .fadeTo(this.arrowOpacityOn, 'arr-right');
 }
 
 Ad.prototype.start = function() {
-    this.fadeOut('overlay-img', this.effects.overlayFadeOut);
-    this.fadeOut('overlay-txt', this.effects.overlayFadeOut);
-    this.slideIn('arr-right', this.effects.arrowsSlideIn);
-    this.slideIn('arr-left', this.effects.arrowsSlideIn);
+    return this
+        .fadeOut('overlay-img', this.effects.overlayFadeOut)
+        .fadeOut('overlay-txt', this.effects.overlayFadeOut);
 }
 
 Ad.prototype.slideIn = function(element, options) {
-    this.animate(element, options.transition, {
+    options = options || {};
+    return this.animate(element, options.transition, {
         transform: 'translateX(0px)'
     }, options.delay);
 }
 
 Ad.prototype.slideOutLeft = function(element, options) {
-    this.animate(element, options.transition, {
+    options = options || {};
+    return this.animate(element, options.transition, {
         transform: 'translateX(-' + this.width + 'px)'
     }, options.delay);
 }
 
 Ad.prototype.slideOutRight = function(element, options) {
-    this.animate(element, options.transition, {
+    options = options || {};
+    return this.animate(element, options.transition, {
         transform: 'translateX(' + this.width + 'px)'
-    }, options.delay);
+    }, options.delay || 0);
 }
 
 Ad.prototype.fadeOut = function(element, options) {
-    this.animate(element, options.transition, {opacity: 0}, options.delay);
+    return this.fadeTo(0, element, options);
+}
+
+Ad.prototype.fadeIn = function(element, options) {
+    return this.fadeTo(1, element, options);
+}
+
+Ad.prototype.fadeTo = function(opacity, element, options) {
+    options = options || {};
+    return this.animate(element, options.transition, {
+        opacity: opacity
+    }, options.delay);
 }
 
 Ad.prototype.animate = function(element, transition, properties, delay) {
     setTimeout(function() {
-        var style = ['transition:all ' + transition];
+        var style = transition ? ['transition:all ' + transition ] : [];
         for (var i in properties)
             style.push(i + ':' + properties[i]);
-        this.setInlineStyle(element, style.join(';'));
-    }.bind(this), delay);
+        window.gwd.actions.events.setInlineStyle(element, style.join(';'));
+    }.bind(this), delay || 0);
+    return this;
+}
+
+Ad.prototype.onRightArrowClick = function() {
+    return this
+        .resetAutoPlay()
+        .navigate(+1);
+}
+
+Ad.prototype.onLeftArrowClick = function() {
+    return this
+        .resetAutoPlay()
+        .navigate(-1);
+}
+
+Ad.prototype.navigate = function(increment) {
+    if (this.busy || (increment < 0 && this.currentFrame < 1))
+        return this;
+    this.setBusy(this.frameDuration);
+
+    // slide out
+    var slideOut = (increment > 0 ? this.slideOutLeft : this.slideOutRight).bind(this);
+    slideOut('pic-' + this.currentFrame, this.effects.framePhotoSlideOut)
+    slideOut('txt-' + this.currentFrame, this.effects.frameTextSlideOut);
+
+    // slide in
+    this.currentFrame += increment;
+    this.slideIn('pic-' + this.currentFrame, this.effects.framePhotoSlideIn)
+        .slideIn('txt-' + this.currentFrame, this.effects.frameTextSlideIn);
+
+    // arrows
+    switch(this.currentFrame) {
+        case 0: return this.fadeTo(this.arrowOpacityOff, 'arr-left', this.effects.arrowLeftFade);
+        case 1: return this.fadeTo(this.arrowOpacityOn, 'arr-left', this.effects.arrowLeftFade);
+        case 4: return this
+            .slideOutArrows()
+            .playLastFrame();
+        default: return this;
+    }
+}
+
+Ad.prototype.setBusy = function(duration) {
+    this.busy = true;
+    setTimeout(function() {
+        this.busy = false;
+    }.bind(this), duration);
+}
+
+Ad.prototype.slideOutArrows = function() {
+    return this
+        .slideOutLeft('arr-left', this.effects.arrowsSlideOut)
+        .slideOutRight('arr-right', this.effects.arrowsSlideOut);
+}
+
+Ad.prototype.playLastFrame = function() {
+    return this
+        .fadeOut('underlay-txt', this.effects.underlayCrossFade)
+        .fadeIn('underlay-logo', this.effects.underlayCrossFade);
 }
